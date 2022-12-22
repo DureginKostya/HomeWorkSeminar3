eca350ee8cbba5ea7b707581a8f8eb3dbd3bc3f2
DureginKostya <kot-502@mail.ru>
2022-12-23 03:39:46 +0500

    Added folder Task019

---
+++ Task019/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число 
+и проверяет, является ли оно палиндромом.*/
+Console.Clear();
+int GetUserValue(string Message)
+{
+    Console.Write(Message);
+    int Result = int.Parse(Console.ReadLine()!);    
+    return Result;
+}
+string TestingNumber(int UserNumber)
+{
+    string Answer = "Число меньше 11 не может быть палиндромом";
+    if (UserNumber > 10)
+    {
+        int CountTen = GetCountTen(UserNumber);
+        Answer = DeterminationNumberPalindrome(UserNumber, CountTen);
+    }    
+    return Answer;
+}
+int GetCountTen(int Number)
+{
+    int count = 0;
+    while (Number > 0)
+    {
+        Number = Number / 10;
+        count++;
+    }  
+    return count;
+}
+string DeterminationNumberPalindrome(int NumberPalindrome, int Order)
+{    
+    string Decision = $"Число {NumberPalindrome} является палиндромом";            
+    int Count = Order / 2;
+    int Index = 0;
+    int Degree = Order - 1;
+    int ProcessedNumber = NumberPalindrome;
+    while (Index < Count)
+    {
+        int DigitFirst = ProcessedNumber / ((int)Math.Pow(10, Degree));
+        int DigitLast = ProcessedNumber % 10;
+        if (DigitFirst != DigitLast)
+        {
+            Decision = $"Число {NumberPalindrome} не является палиндромом";
+            break;
+        }
+        ProcessedNumber = ProcessedNumber % ((int)Math.Pow(10, Degree));
+        ProcessedNumber = ProcessedNumber / 10;
+        Degree = Degree - 2;
+        Index++;        
+    }    
+    return Decision;
+}
+int EnterNumber = GetUserValue("Введите число: ");
+string Palindrome = TestingNumber(EnterNumber);
+Console.WriteLine(Palindrome);
