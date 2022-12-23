24847d040d73fa555b2c2b6d0f9d0357a845983f
DureginKostya <kot-502@mail.ru>
2022-12-23 12:20:04 +0500

    Changed file in folder Task021

--- Task021/Program.cs
+++ Task021/Program.cs
@@ -1,18 +1,16 @@
 ﻿/*Напишите программу, которая принимает на вход координаты двух точек
-и находит расстояние между ними в 3D пространстве.
-A (3,6,8); B (2,1,-7), -> 15.84
-A (7,-5, 0); B (1,-1,9) -> 11.53*/
+и находит расстояние между ними в 3D пространстве.*/
 Console.Clear();
-void GetUserValue(int[,] MatrizaPoints, int count)
+void GetUserValue(int[,] MatrixPoints, int Count)
 {
-    for (int i = 0; i < count; i++)
+    for (int i = 0; i < Count; i++)
     {
         Console.WriteLine($"Введите координаты точки № {i + 1}:");
-        CreateArray(MatrizaPoints, i);
+        CreateMatrix(MatrixPoints, i);
         Console.WriteLine();
     }
 }
-void CreateArray(int[,] Matriza, int Index)
+void CreateMatrix(int[,] Matrix, int Index)
 {
     for (int i = 0; i < 3; i++)
     {
@@ -21,38 +19,37 @@ void CreateArray(int[,] Matriza, int Index)
             case 0:
             {
                 Console.Write("ось X: ");
-                Matriza[Index, i] = int.Parse(Console.ReadLine()!);
+                Matrix[Index, i] = int.Parse(Console.ReadLine()!);
                 break;
             }
             case 1:
             {
                 Console.Write("ось Y: ");
-                Matriza[Index, i] = int.Parse(Console.ReadLine()!);
+                Matrix[Index, i] = int.Parse(Console.ReadLine()!);
                 break;                
             }
             case 2:
             {
                 Console.Write("ось Z: ");
-                Matriza[Index, i] = int.Parse(Console.ReadLine()!);
+                Matrix[Index, i] = int.Parse(Console.ReadLine()!);
                 break;
             }
        } 
     }
 }
-double CalculationDistance(int[,] CalculationMatriza)
+double Distance(int[,] MatrixCoordinates)
 {
     int Sum = 0;    
     int Diff = 0;
     for (int i = 0; i < 3; i++)
     {
-        Diff =  CalculationMatriza[1, i] - CalculationMatriza[0, i];
+        Diff =  MatrixCoordinates[1, i] - MatrixCoordinates[0, i];
         Sum = Sum + (int)Math.Pow(Diff, 2);
     }
-    double Distance = Math.Sqrt(Sum);
-    return Distance;
+    double Spacing = Math.Sqrt(Sum);
+    return Spacing;
 }
-int[,] CoordinatesPoints = new int[2, 3];
-GetUserValue(CoordinatesPoints, 2);
-double DistancePoints = Math.Round(CalculationDistance(CoordinatesPoints), 2);
-Console.WriteLine($"Расстояние между точкой № 1 (координаты: X={CoordinatesPoints[0, 0]}; Y={CoordinatesPoints[0, 1]}; Z={CoordinatesPoints[0, 2]}) и точкой № 2 (координаты: X={CoordinatesPoints[1, 0]}; Y={CoordinatesPoints[1, 1]}; Z={CoordinatesPoints[1, 2]}) равно {DistancePoints}");
-
+int[,] TwoPoints = new int[2, 3];
+GetUserValue(TwoPoints, 2);
+double DistancePoints = Math.Round(Distance(TwoPoints), 3);
+Console.WriteLine($"Расстояние между точкой № 1 (координаты: X={TwoPoints[0, 0]}; Y={TwoPoints[0, 1]}; Z={TwoPoints[0, 2]}) и точкой № 2 (координаты: X={TwoPoints[1, 0]}; Y={TwoPoints[1, 1]}; Z={TwoPoints[1, 2]}) равно {DistancePoints}");
