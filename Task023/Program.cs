3c45e47125b5347a0cdb6b7343862363aaebd22e
DureginKostya <kot-502@mail.ru>
2022-12-23 05:32:16 +0500

    Changed files in folder Task023

--- Task023/Program.cs
+++ Task023/Program.cs
@@ -1,6 +1,31 @@
 ﻿/*Напишите программу, которая принимает на вход число (N)
-и выдаёт таблицу кубов чисел от 1 до N.
-3 -> 1, 8, 27
-5 -> 1, 8, 27, 64, 125*/
+и выдаёт таблицу кубов чисел от 1 до N.*/
 Console.Clear();
+int GetUserValue(string Message)
+{
+    Console.Write(Message);
+    int Value = int.Parse(Console.ReadLine()!);
+    return Value;
+}
+void TableValue(int UserNumber)
+{
+    if (UserNumber > 1)
+    {
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine($"Числа от 1 до {UserNumber} || Числа в кубе ");
+        Console.WriteLine("---------------------------------");
+        for (int i = 1; i <= UserNumber; i++)
+        {
+            int Number = (int)Math.Pow(i, 3);
+            Console.WriteLine($"        {i}       ||       {Number} ");
+            Console.WriteLine("---------------------------------");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введено недопустимое число, N должно быть больше единицы");
+    }
+}
 
+int Number = GetUserValue("Введите число: ");
+TableValue(Number);
